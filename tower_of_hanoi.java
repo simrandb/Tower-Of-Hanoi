42b39e4af28c1a7609e956ddd62db2009e60b8e1
simrandb <61749607+simrandb@users.noreply.github.com>
2020-07-07 00:43:42 +0530

    Update tower_of_hanoi.java

--- tower_of_hanoi.java
+++ tower_of_hanoi.java
@@ -1,35 +1,32 @@
-import java.util.*;
+import java.util.Scanner;
 
-class tohh
-{
-    int cnt=0;
-    void toh(int n,char f,char t,char a)
-{
-    if (n==1)
+public class TowerOfHanoi
     {
-        System.out.println("Step no "+(++cnt)+": Move disc 1 from "+f+" to "+t+".");
-        return ;
+    
+    static int cnt=0;
+    static Scanner scan= new Scanner(System.in);
+    static void toh(int n,char f,char t,char a)
+        {
+            if (n==1)
+            {
+                System.out.println("Step no "+(++cnt)+": Move disc 1 from "+f+" to "+t+".");
+                return ;
+            }
+            else
+            {
+                toh(n-1,f,a,t);
+                System.out.println("Step no "+(++cnt)+": Move disc "+n+" from "+f+" to "+t+".");
+                toh(n-1,a,t,f);
+            }
+        }
+
+
+        public static void main(String []args)
+        {
+            int n=0;
+            System.out.println("Enter no. of rings:");
+            n=scan.nextInt();
+            char f='A',t='C',a='B';
+            toh(n,f,t,a);
+        }
     }
-    else
-    {
-        toh(n-1,f,a,t);
-        System.out.println("Step no "+(++cnt)+": Move disc "+n+" from "+f+" to "+t+".");
-        toh(n-1,a,t,f);
-    }
-}
-}
-public class HelloWorld{
-
-
-private static      final   Scanner s=new Scanner (System.in);
-
-     public static void main(String []args){
-         System.out.println("Enter no. of rings:");
-        
-
-         char f='f',t='t',a='a';
-         tohh h =new tohh();
-         
-         h.toh(5,f,t,a);
-     }
-}
